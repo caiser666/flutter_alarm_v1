bdf845c1467fbcbd9d465ea327c2190b56a18ed6
caiser666 <chailirsentosa3696@gmail.com>
2021-11-14 14:00:50 +0700

    Update

--- lib/app/modules/home/views/alarm_info_view.dart
+++ lib/app/modules/home/views/alarm_info_view.dart
@@ -36,7 +36,7 @@ class AlarmInfoView extends GetView<HomeController> {
                       constraints: BoxConstraints(
                         minHeight: 196,
                         maxWidth: Get.width,
-                        maxHeight: 320,
+                        maxHeight: 32,
                       ),
                       width: Get.width,
                       child: controller.myAlarmInfoList.length > 0
